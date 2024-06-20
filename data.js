9f52ba6f64ef7a50aaa53b718c9fbf0ad38a094a
Foxhillsmedia <91200585+Foxhillsmedia@users.noreply.github.com>
2024-06-20 11:09:48 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01openingpage",
+      "name": "01OpeningPage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.2029718408840555,
+        "pitch": -0.003260864550270526,
+        "fov": 1.28151369453833
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.912441297573123,
+          "pitch": 0.2013379032529059,
+          "rotation": 5.497787143782138,
+          "target": "1-02-juniorgates"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02-juniorgates",
+      "name": "02 JuniorGates",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.6345806380887566,
+        "pitch": 0.015515304634883265,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9329674382749626,
+          "pitch": 0.10968327249111276,
+          "rotation": 13.351768777756625,
+          "target": "0-01openingpage"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Foxhills Federation",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
